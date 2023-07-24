f01565b6b65ac7157c57a5259e24dcb3e4d0da08
ritiksharmarj <theonlyritik@gmail.com>
2023-07-24 15:45:54 +0530

    loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { tempMovieData, tempWatchedData } from './data';
+
 import NavBar from './components/NavBar';
 import Box from './components/Box';
 import MovieList from './components/MovieList';
 import WatchedSummary from './components/WatchedSummary';
 import WatchedMovieList from './components/WatchedMovieList';
 import Main from './components/Main';
+import Loader from './components/Loader';
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      setIsLoading(true);
+
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${
+          import.meta.env.VITE_OMDB_API_KEY
+        }&s=batman`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+
+      setIsLoading(false);
+    };
+    fetchMovies();
+  }, []);
 
   return (
     <>
       <NavBar movies={movies} />
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
